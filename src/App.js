a67cc00fd8d34077ddf011a0f03bd906aed6d2ae
Benny Thomas <ben.jameen03@gmail.com>
2022-09-20 11:42:04 +0530

    complete quiz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Intro from "./components/Intro"
+import Quiz from "./components/Quiz"
+import Blob from "./components/Blob"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [quiz, setQuiz] = React.useState(false)
+
+    const getQuiz = () => {
+        setQuiz(!quiz)
+    }
+
+    return (
+        <main>
+            <Blob position="top"/>
+            { quiz ? <Quiz /> : <Intro getNewQuiz={getQuiz}/> }
+            <Blob position="bottom"/>
+        </main>
+    )
 }
-
-export default App;
---
+++ src/components/Blob.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Blob(props) {
+    return (
+        <div className={`blob ${props.position}`}></div>
+    )
+}
